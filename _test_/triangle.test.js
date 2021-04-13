44e2e478b7bcc2e3acf6ea68a637d6148c62225a
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-13 13:17:56 -0700

    add coverage to jest tester.

--- _test_/triangle.test.js
+++ _test_/triangle.test.js
@@ -1,5 +1,18 @@
 import Triangle from './../src/triangle.js';
 
+// describe('Triangle', () => {
+//   let reusableTriangle;
+
+//   beforeEach(() => {
+//     reusableTriangle = new Triangle(5, 5, 5);
+//   });
+
+//   test('should show how beforeEach() works', () => {
+//     console.log(reusableTriangle);
+//   });
+// });
+
+
 describe('Triangle', () => {
 
   test('should correctly create a triangle object with three lengths', () => {
